9ae8d43a82b2b9eb236aa8f1c2807d3d92a8b8ba
huarui <huarui.wu@gmail.com>
2017-11-17 13:58:38 -0800

    add index page

--- src/views/IndexView.js
+++ src/views/IndexView.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class IndexView extends Component {
     render() {
-        return <h1>Code Together</h1>
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginTop: '50%'
+                }}
+            >
+                <div
+                    style={{
+                        maxWidth: '50%'
+                    }}
+                >
+                    <h1>Code Together</h1>
+                </div>
+                <div
+                    style={{
+                        maxWidth: '50%'
+                    }}
+                >
+                    <Link to="/">
+                        <button>NEW EDITOR</button>
+                    </Link>
+                </div>
+            </div>
+        )
     }
 }
 
