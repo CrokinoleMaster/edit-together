ea8f71d8742611c58df3cb8950e80197f11e014f
huarui <huarui.wu@gmail.com>
2017-11-17 16:42:59 -0800

    add config

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: 'AIzaSyAT0KuflYXkIN_-L40kS0A6lzfpgR2Wmq4',
+    authDomain: 'code-edit-together.firebaseapp.com',
+    databaseURL: 'https://code-edit-together.firebaseio.com',
+    projectId: 'code-edit-together',
+    storageBucket: 'code-edit-together.appspot.com',
+    messagingSenderId: '935354614920'
+}
